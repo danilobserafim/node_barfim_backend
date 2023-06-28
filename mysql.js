87d3d09e3d4530fc350416d3043bd2bad8f464e3
Danilo Serafim <danilob.serafim@gmail.com>
2023-06-28 04:33:14 -0300

    resolvendo bug

--- mysql.js
+++ mysql.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 var pool = mysql.createPool({
   user: process.env.USER,
-  password: process.env.PASSWORD || "",
+  password: process.env.PASSWORD,
   database:process.env.DATABASE,
   host: process.env.HOST,
   port: 3306,
