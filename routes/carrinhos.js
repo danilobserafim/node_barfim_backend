e5a2f726f2ac134dc89d149d7e48728fcd4b16b3
Danilo Serafim <danilob.serafim@gmail.com>
2023-06-29 11:19:35 -0300

    Ultima tentativa antes de ir dormir

--- routes/carrinhos.js
+++ routes/carrinhos.js
@@ -9,10 +9,11 @@ require("dotenv").config()
 
 router.post("/", (req, res, next)=>{
     const {venda_id, produto_id} = req.body
+
     mysql.getConnection((error, conn) => {
         try {
           conn.query(`INSERT INTO carrinhos (venda_id, produto_id) VALUES (?, ?)`, 
-          [venda_id, produto_id],
+          [parseInt(venda_id), parseInt(produto_id)],
           (erro, result, field) => {
             conn.release();
             if (erro) {
