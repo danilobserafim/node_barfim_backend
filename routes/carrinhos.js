f76f1cb8cd661c9af625ae54396a748f674ba9c7
Danilo Serafim <danilob.serafim@gmail.com>
2023-07-01 03:41:29 -0300

    não consegui terminar a noite

--- routes/carrinhos.js
+++ routes/carrinhos.js
@@ -8,20 +8,22 @@ require("dotenv").config()
 
 
 router.post("/", (req, res, next)=>{
-    const {venda_id, produto_id} = req.body
+    const {venda_id, carrinho} = req.body
     mysql.getConnection((error, conn) => {
         try {
-          conn.query(`INSERT INTO carrinhos (venda_id, produto_id) VALUES (?, ?)`, 
-          [venda_id, produto_id],
-          (erro, result, field) => {
-            conn.release();
-            if (erro) {
-              return res.status(500).send({
-                message: "nada encontrado",
-              });
-            }
-            res.status(200).send(result);
-          });
+          carrinho.map(produto=>{
+            conn.query(`INSERT INTO carrinhos (venda_id, produto_id) VALUES (?, ?)`, 
+            [venda_id, produto.produto_id],
+            (erro, result, field) => {
+              conn.release();
+              if (erro) {
+                return res.status(500).send({
+                  message: "nada encontrado",
+                });
+              }
+              res.status(200).send(result);
+            });
+          })
         } catch (error) {
           res.status(500).send({
             message: "algo deu errado",
