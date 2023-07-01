7754d22db805593955809464c006dab4bdc308ee
Danilo Serafim <danilob.serafim@gmail.com>
2023-07-01 05:03:08 -0300

    Finalizando o carrinho

--- routes/carrinhos.js
+++ routes/carrinhos.js
@@ -9,30 +9,25 @@ require("dotenv").config()
 
 router.post("/", (req, res, next)=>{
     const {venda_id, carrinho} = req.body
-    arrayCarrinho = JSON.parse(carrinho)
-    mysql.getConnection((error, conn) => {
-      try {
-        arrayCarrinho.map(produto=>{
-          conn.query(`INSERT INTO carrinhos (venda_id, produto_id) VALUES (?, ?)`, 
-          [venda_id, produto.id],
-          (erro, result, field) => {
-            if (erro) {
-              return res.status(500).send({
-                message: "nada encontrado",
-              });
-            }
-            res.status(200).send(result);
-          });
-        })
-        conn.release();
-        } catch (error) {
-        conn.release();
-          res.status(500).send({
-            message: "algo deu errado",
-          });
-
-        }
-      });
+    carrinho.map(produto=>{
+      mysql.getConnection((error, conn) => {
+        try {
+            conn.query(`INSERT INTO carrinhos (venda_id, produto_id) VALUES (?, ?)`, 
+            [venda_id, produto.id],
+            (erro, result, field) => {
+              
+            });
+          } catch (error) {
+            res.status(500).send({
+              message: "algo deu errado",
+            });
+  
+          }
+        });
+    })
+    res.status(200).send({
+      message: "tudo certo campeão"
+    })
   })
   
 
