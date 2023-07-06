bff83fd22b60e255ab46dc3a9b93b029b7a9d569
Danilo Serafim <danilob.serafim@gmail.com>
2023-07-06 03:37:26 -0300

    corrigindo rota de categorias

--- routes/categorias.js
+++ routes/categorias.js
@@ -28,7 +28,7 @@ router.get("/:id", (req, res, next)=>{
     const id = req.params.id
     mysql.getConnection((error, conn)=>{
         try {
-            conn.query(`SELECT nome_produto, descricao_produto, valor_produto, nome_categoria FROM categorias
+            conn.query(`SELECT id_produto, nome_produto, descricao_produto, valor_produto, quantidade_produto, nome_categoria FROM categorias
                         INNER JOIN produtos ON categoria_id = id_categoria 
                         WHERE id_categoria = ? `, [id], (erro, result)=>{
                 conn.release()
